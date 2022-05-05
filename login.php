76bf2eb9f264d6424f07192f2c675ff10b880fbc
Tejswarup-Ravuri <101380213+Tejswarup-Ravuri@users.noreply.github.com>
2022-05-05 22:33:16 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,116 @@
+<?php
+	session_start();
+	if($_SESSION["fname"] == "")
+	{
+		//header("Location: index.php");
+	}
+	else
+	{
+		header("Location: home.php");
+	}
+?>
+
+<html>
+<head>
+	<title>Chatting application- 2021</title>
+	<script type="text/javascript">
+		function redirr()
+		{
+			location.href = "index.php";
+		}
+	</script>
+</head>
+
+<body>
+<center>
+	<div id="main">
+		<h1>
+			<font style="color:red; font-family:Calibri;">Online</font>Chat
+		</h1>
+		<h4>
+			<?php
+				if($_SESSION['reg-success'] == "yes" && $_SESSION['login-failed'] == "clear" )
+				{
+					echo("/registration successful");
+				}
+				if($_SESSION['login-failed'] == "yes" && $_SESSION['reg-success'] = "clear")
+				{
+					echo("Login failed. Username and password not found");
+				}
+			?>
+		</h4>
+		<form action="confirm-login.php" method="POST">
+			<table>
+			<tr>
+				<td>Username</td><td>:</td><td><input class="txt" type="text" required name="username" /></td>
+			</tr>
+			<tr>
+				<td>Password</td><td>:</td><td><input class="txt" type="password" required name="password" /></td>
+			</tr>
+			<tr>
+				<td colspan = "3" align="center">
+					<a href="register.php"><input id="reg" type="button" value="register" /></a>
+					<input id="login" type="submit" value="log-in" />
+				</td> 
+			</tr>
+		</form>
+	</div>
+</center>
+</body>
+
+</html>
+
+<style>
+
+h2
+{
+	color:green;
+}
+.txt
+{
+	border-radius:4px;
+	border:1px;
+	height:30px;
+}
+#a
+{
+	text-decoration:none;
+}
+
+#reg,#login
+{
+	width:110px;
+	height:30px;
+	border-radius:5px;
+	border:0px;
+	color:white;
+}
+
+#reg
+{
+	background:orange;
+
+}
+
+#login
+{
+	background:skyblue;
+}
+
+
+#main
+{
+	height:auto;
+	width:400px;
+	border:2px white solid;
+	float:center;
+	margin-top:120px;
+}
+body
+{
+	background: url(Chatimages/home.jpg);
+	color:white;
+	background-size: 1370px 620px;
+}
+</style>
+
